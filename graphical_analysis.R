b96bf4ebdb6ee3b62a139e0f9ee95c5a93a69f14
agrawalsourabh <agrawal.sourabh644@gmail.com>
2019-06-21 01:20:45 +0530

    updated

--- graphical_analysis.R
+++ graphical_analysis.R
@@ -23,3 +23,22 @@ ggplot(data = our.train, mapping = aes(x = Sex, fill = Survived)) +
   geom_bar() +
   ggtitle("Sex") +
   stat_count(aes(label = ..count..), geom = "label")
+
+# TITLE
+# creating word cloud
+title = data.frame(table(our.data$title))
+wordcloud(title$Var1, freq = title$Freq, colors = brewer.pal(8, "Accent"), random.order = T)
+
+# bar graph for missing value as per title
+ggplot(data = missing_age_title.df, mapping = aes(x = reorder(title, Perc), y = Perc)) +
+  geom_bar(fill = "#ed8661", stat = "identity") +
+  coord_flip() +
+  geom_label(label = paste(missing_age_title.df$Perc, "%")) +
+  ggtitle("Age missing value") +
+  xlab("Title") +
+  ylab("Percentage (%)")
+
+# word cloud for missing_age_title
+wordcloud(missing_age_title.df$title, freq = missing_age_title.df$Age, 
+          colors = brewer.pal(8, "Accent"), random.order = T)
+
