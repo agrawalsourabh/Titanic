70eceed192a34bf1b2274db634555153e542cee6
agrawalsourabh <agrawal.sourabh644@gmail.com>
2019-06-21 01:21:03 +0530

    updated

--- functions.R
+++ functions.R
@@ -20,3 +20,17 @@ missing_val = function(x){
   
   return(missing.df)
 }
+
+# find title using name column
+getTitle = function(x){
+  title = c()
+  
+  for (i in 1:length(x)) {
+    m = str_split_fixed(x[i], pattern = ",", n = 2)
+    t = str_split_fixed(m[2], pattern = ". ", n = 2)
+    
+    title = c(title, t[1])
+  }
+  
+  return(title)
+}
