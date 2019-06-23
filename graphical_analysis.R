1107954264a8c8af66bef4474482300eeae80db3
agrawalsourabh <agrawal.sourabh644@gmail.com>
2019-06-23 17:53:35 +0530

    modified

--- graphical_analysis.R
+++ graphical_analysis.R
@@ -42,3 +42,45 @@ ggplot(data = missing_age_title.df, mapping = aes(x = reorder(title, Perc), y = 
 wordcloud(missing_age_title.df$title, freq = missing_age_title.df$Age, 
           colors = brewer.pal(8, "Accent"), random.order = T)
 
+# family.size
+# Bar Graph for family.size fill by survival
+
+ggplot(data = our.data[-(419:1309), ], mapping = aes(x = family.size, fill = Survived)) +
+  geom_bar() +
+  ggtitle("Survival - Family Size") +
+  stat_count(aes(label = ..count..), geom = "label")
+
+# non survival percentage per family size
+ggplot(data = family.size_survived_ft.df, mapping = aes(x = reorder(family.size, -ns.perc), 
+                                                        y = ns.perc)) +
+  geom_bar(stat = "identity", fill="#a7f442") +
+  coord_flip() +
+  ggtitle("Non Survival %") +
+  xlab("Family Size") +
+  ylab("Percentage") +
+  geom_label(label = paste(family.size_survived_ft.df$ns.perc, "%"))
+
+# Embarked
+ggplot(data = our.data[-(419:1309), ], mapping = aes(x = Embarked, fill = Survived)) +
+  geom_bar() +
+  ggtitle("Survival - Embarked") +
+  stat_count(aes(label = ..count..), geom = "label")
+
+#
+#   WORKPLACE SAVED
+#
+
+# non survival percentage as per embarked
+ggplot(data = embarked_survived.ft.df, 
+       mapping = aes(x = reorder(Embarked, -NS.Perc), y = NS.Perc)) +
+  geom_bar(stat = "identity", fill = "#f49542", alpha = 0.7) +
+  coord_flip() +
+  ggtitle("Non Survival Percentage as per Embarked") +
+  xlab("Embarked") +
+  ylab("Non Survival Percentage") + 
+  geom_label(label = paste(embarked_survived.ft.df$NS.Perc, "%")) +
+  scale_y_continuous(limits = c(0, 100))
+
+#
+#  ============================== WORKPLACE SAVED ========================================
+#
